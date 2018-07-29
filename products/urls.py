36215158c148791de3190b4cc8d937ebd1b83b00
kurianbenoy <kurian.pro@gmail.com>
2018-07-29 09:50:58 +0530

    fix url bug for Product Condition detail

--- products/urls.py
+++ products/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     url(r'^(?P<pk>[0-9]+)/$',ProductDetail.as_view(),name='ProductDetail'),
     url(r'^search/$',ProductListAPI.as_view(),name='searchstuff'),
     url(r'^condition/$',ProductConditionList.as_view(),name='ProductCondition'),
-    url(r'^condition/(?P<pk>[0-9]+)/$',ProductDetail.as_view(),name='ProductDetail'),
+    url(r'^condition/(?P<pk>[0-9]+)/$',ProductConditionDetail.as_view(),name='ProductDetail'),
 
 ]
